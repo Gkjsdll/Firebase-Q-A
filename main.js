820347a0a7d0524c94591ead1228dc3641f1cd5d
Zack Winchell <zackwinchell@gmail.com>
2016-01-19 00:56:12 -0800

    added comments

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 var mainRef = new Firebase("https://gkjsdll-firebase-q-a.firebaseio.com/");
 
-var $newTitle, $newContent, $listings, $mainDiv, $itemInfo;
+var $newTitle, $newContent, $listings, $mainDiv, $itemInfo, $itemComment;
 
 $(document).ready(function(){
-  $('#addListing button').click(createListing);
+  $('#addListing').on("submit", createListing);
   $newTitle = $('#newTitle');
   $newContent = $('#newContent');
   $listings = $("#listings");
   $mainDiv = $("#mainDiv");
   $itemInfo = $("#itemInfo");
-  $itemInfo.click(hideItemInfo);
+  $itemComment = $("#itemComment");
+  $comments = $("#comments");
+  $itemInfo.on("click", "span", hideItemInfo);
   $listings.on("click", "div", showListing);
   mainRef.on("value", updateListings);
+  $('#comment').on("submit", addComment);
 });
 
-function createListing(){
-  var listing = {title: "", content: ""};
+function createListing(e){
+  e.preventDefault();
+  var listing = {title: "", content: "", time: moment().format("h:mma MMM DD"), comments: []};
   listing["title"] = $newTitle.val();
   listing["content"] = $newContent.val();
+  listing["comments"] = [];
   mainRef.push(listing);
 }
 
 function updateListings(listings){
   var allListings = [];
   listings.forEach(function(childSnapshot){
-    debugger;
     var data = childSnapshot.val();
-    var listItem = $("<div>").text(data.title + " – " + data.content).data("key", childSnapshot.key());
+    var listItem = $("<div>").text(data.title + " – " + childSnapshot.val().time)
+                             .data("key", childSnapshot.key()); //I would prefer to pull the data from firebase
     allListings.push(listItem);
   });
   $listings.empty();
@@ -34,8 +39,15 @@ function updateListings(listings){
 };
 
 function showListing(e){
-  debugger;
-  var data = mainRef.child($(this).data("key")).toString();
+  var listing = $(this).data("key");
+  mainRef.child(listing).once("value", function(snap){
+    debugger;
+    $itemInfo.find("#title").text(snap.val().title);
+    $itemInfo.find("#infoContent").text(snap.val().content);
+    $itemInfo.find("#time").text(snap.val().time);
+    $itemInfo.data("key", snap.key());
+  });
+  popComments($itemInfo.data("key"));
   $itemInfo.show();
   $mainDiv.hide();
 }
@@ -43,4 +55,29 @@ function showListing(e){
 function hideItemInfo(){
   $itemInfo.hide();
   $mainDiv.show();
+}
+
+function addComment(e){
+  e.preventDefault();
+  var listing = $(this).closest("#itemInfo").data("key");
+  var comment = {comment: $("#itemComment").val()};
+  mainRef.child(listing + '/messages').push(comment);
+  popComments($(this).closest('#itemInfo').data("key"));
+}
+
+function popComments(baseKey){
+  var comments = [];
+
+  mainRef.child(baseKey+"/messages").once("value", function(babySnap){ //add if statement, use array to prevent duplicate listeners
+    if(babySnap.val()){
+      $.each(babySnap.val(), function(key){
+        mainRef.child(baseKey+"/messages/"+key).once("value", function(x){
+          var comment = $('<li>').text(x.val().comment);
+          comments.push(comment);
+        });
+      });
+    }
+  });
+  $comments.empty();
+  $comments.append(comments);
 }
