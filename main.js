6b95d0cf8e00f489eff73fda4c2425afc5cf53d2
Zack Winchell <zackwinchell@gmail.com>
2016-01-18 21:09:00 -0800

    listings added to firebase now automatically populate the page

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var mainRef = new Firebase("https://gkjsdll-firebase-q-a.firebaseio.com/");
 
-var $newTitle, $newContent;
+var $newTitle, $newContent, $listings;
 
 $(document).ready(function(){
   $('#addListing button').click(createListing);
   $newTitle = $('#newTitle');
   $newContent = $('#newContent');
+  $listings = $("#listings");
+  $listings.on("click", "div", showListing);
+  mainRef.on("value", updateListings);
 });
 
 function createListing(){
@@ -14,3 +17,16 @@ function createListing(){
   listing["content"] = $newContent.val();
   mainRef.push(listing);
 }
+
+function updateListings(listings){
+  var allListings = [];
+  listings.forEach(function(childSnapshot){
+    debugger;
+    var data = childSnapshot.val();
+    var listItem = $("<div>").text(data.title + " – " + data.content)
+                            .data("key", childSnapshot.key());
+    allListings.push(listItem);
+  });
+  $listings.empty();
+  $listings.append(allListings);
+};
